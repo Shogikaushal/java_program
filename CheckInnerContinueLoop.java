236a6e59572ae8d792472a7c76db78b7a3f0528a
Shogikaushal <shogi.xworkz@gmail.com>
2021-08-02 06:13:09 -0700

    Inner continue loop check

---
+++ CheckInnerContinueLoop.java
@@ -0,0 +1,15 @@
+class CheckInnerContinueLoop{
+public static void main(String args[]){
+			for(int i=0;i<=5;i++){
+			System.out.println("i ="+i);
+				{ 
+				for(int j=0;j<=4;j++){
+				if(j==2){
+				continue;
+					}
+				System.out.println("j="+j);
+					}
+
+		}	}
+					}
+				}
